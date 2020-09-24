345828ba8140a92b3d40eba69a880563ee5a0911
imjchiang <jchiang296@gmail.com>
2020-09-24 12:27:21 -0700

    [test_calc.py] subtract test calc class and test_subtract function and pass test

--- test_calc.py
+++ test_calc.py
@@ -7,6 +7,10 @@ class TestCalc(unittest.TestCase):
         self.assertEqual(calc.add(10, 5), 15)
         self.assertEqual(calc.add(100, -1), 99)
         self.assertEqual(calc.add(10, 10), 20)
+    def test_subtract(self):
+        self.assertEqual(calc.subtract(10, 5), 5)
+        self.assertEqual(calc.subtract(100, -1), 101)
+        self.assertEqual(calc.subtract(10, 10), 0)
 
 if __name__ == "__main__":
     unittest.main()
